e9f510e246fb79d3d640a973320c5f5709fc9903
ddailey <deandail@gmail.com>
2018-02-15 19:08:07 -0800

    Fully anonymized

--- clean-capstone-data.R
+++ clean-capstone-data.R
@@ -11,3 +11,15 @@ customers_export <- customers_export[!(customers_export$Email == "NA"),]
 
 #Join the tables together, matching emails, and keeping all data from both tables
 customer_data_full <- join(full_email_list, customers_export, by = "Email")
+
+#Add a unique ID to each row
+customer_data_full$ID <- 1:nrow(customer_data_full)
+write_csv(customer_data_full, "customers_with_names.csv")
+
+#Remove identifying info
+customer_data_full$Email <- NULL
+customer_data_full$`First Name` <- NULL
+customer_data_full$`Last Name` <- NULL
+
+#Save anonymized data
+write_csv(customer_data_full, "email-list-anon.csv")
